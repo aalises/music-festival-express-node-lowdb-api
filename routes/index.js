ca591a1d75a0b4b5519ac2ca21492135b4f42a61
Albert Alises <albert.alises@gmail.com>
2018-08-01 18:19:26 +0200

    Added JSON responses to PUT

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,14 @@ router.use('/festivals', festivalRouter);
 //Middleware called after the route has been resolved and the object with all the info populated
 router.use('*',(req,res) => {  
     const queryObject = req.app.get('search_query');
-    res.json({...queryObject, method: req.method});
     switch(req.method){
         case 'PUT':
-            if(!queryObject.festival_id) console.log("Cannot write: Festival name is mandatory")
-            else dbInstance.write(queryObject);
+            if(!queryObject.festival_id) res.json({success: 0, error: "Cannot write: Festival name is mandatory" });
+            else dbInstance.write(queryObject).then(() => {
+                res.json({success: 1, message: "The entry has been succesfully added to the database" });
+            }).catch(err => {  
+                res.json({success: 0, message: err });
+            });
         break;
     }
 });
